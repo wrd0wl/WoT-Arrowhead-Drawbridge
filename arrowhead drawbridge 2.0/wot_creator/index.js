aac667b96f92ed83fcb334212b6ed82b0711a52c
wrd0wl <36873401+wrd0wl@users.noreply.github.com>
2022-06-20 13:29:02 +0200

    Create index.js

---
+++ arrowhead drawbridge 2.0/wot_creator/index.js
@@ -0,0 +1,17 @@
+let jsonDevices = require("./devices.json");
+
+const axios = require('axios').default;
+
+const wot = require("./wot.js");
+
+const factory = require('./ahFactory.js');
+
+const requests = require('./requests.js');
+const ahFactory = require("./ahFactory.js");
+
+startWot = async () =>{
+    for(var i = 0; i < jsonDevices.length; i++){
+        await wot(jsonDevices[i], i + 1);
+        await requests(ahFactory(jsonDevices[i].selector, i + 1));
+    }
+}

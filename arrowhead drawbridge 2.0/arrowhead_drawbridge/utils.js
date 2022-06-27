5e516294e8dda74041f0d9d1a4e683d7f678a5c1
wrd0wl <36873401+wrd0wl@users.noreply.github.com>
2022-06-27 14:09:25 +0200

    Delete arrowhead drawbridge 2.0 directory

--- arrowhead drawbridge 2.0/arrowhead_drawbridge/utils.js
+++
@@ -1,6 +0,0 @@
-const checkMetadata = (data) =>{
-    return data.includes('building') || data.includes('floor') || data.includes('room') ? true : false;
-        
-}
-
-module.exports = checkMetadata;

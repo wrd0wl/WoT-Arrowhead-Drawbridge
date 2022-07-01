6eed07082f8159d3062a6852c4056fa493d625ee
Oksana <oksana.mytsavka@studio.unibo.it>
2022-07-01 16:46:21 +0200

    update

--- src/wot_creator/requests.js
+++ src/wot_creator/requests.js
@@ -1,13 +1,13 @@
 const axios = require('axios').default;
 
-const conf = require('../config.json');
+const config = require('../config.json');
 
 const getAH = async () =>{
-    return await axios.get(`http://${conf.arrowhead.host}:${conf.arrowhead.port}/serviceregistry/query/all`);
+    return await axios.get(`http://${config.arrowhead.host}:${config.arrowhead.port}/serviceregistry/query/all`);
 }
 
 const postToAHS = async (body) =>{
-    await axios.post(`http://${conf.arrowhead.host}:${conf.arrowhead.port}/serviceregistry/register`, body, {headers:{
+    await axios.post(`http://${config.arrowhead.host}:${config.arrowhead.port}/serviceregistry/register`, body, {headers:{
         'Content-Type': 'application/json'}
     });
 }

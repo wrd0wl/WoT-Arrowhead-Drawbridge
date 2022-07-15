ce4ef8e83d826578226e728d5219914bb2d8275d
Oksana <oksana.mytsavka@studio.unibo.it>
2022-07-15 09:52:50 +0200

    update config.json

--- src/arrowhead_drawbridge/api.js
+++ src/arrowhead_drawbridge/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const config = require('../config.json');
+const config = require('./config.json');
 
 const control = require('./control.js');
 
